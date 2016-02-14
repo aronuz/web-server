279d4535dd5b7f5817411d636a0f643b9bac510e
Anatoliy <Anatoliy.kogan@gmail.com>
2016-02-14 11:34:10 -0500

    update pot for heroku

---
+++ server3.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app=express();
+var PORT = 3000;
+
+var middleware=require('./middleware.js');
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(req, res){
+  res.send('about us');
+});
+
+app.use(express.static(__dirname+'/public'));
+// console.log(__dirname+'/public');
+
+app.listen(PORT, function(){
+  console.log('server started on port '+PORT)
+});
